9738490cd323ecd0d5bd4897df6abefb714cd969
DarshanGowda33 <darshangowdakr33@gmail.com>
2025-10-16 23:00:13 +0530

    used array for marks

--- performanceAnalyzer.c
+++ performanceAnalyzer.c
@@ -7,23 +7,11 @@ struct studentDetails
 {
     int roll_num;
     char name[50];
-    int marks1;
-    int marks2;
-    int marks3;
+    int marks[3];
     int total;
     float average_marks;
     char grade;
 };
-
-int computeTotalMarks(struct studentDetails student)
-{
-    return (student.marks1+student.marks2+student.marks3);
-}
-
-float computeAverageMarks(struct studentDetails student)
-{
-    return (student.marks1+student.marks2+student.marks3)/3.0;
-}
 
 void computeGrade(struct studentDetails *student)
 {
@@ -51,38 +39,46 @@ void computeGrade(struct studentDetails *student)
 
 void computePerformance(char grade)
 {
-    if (grade == 'A')
+    int countOfStars = 0;
+    switch(grade)
     {
-        printf("Performance : *****\n");
+        case 'A': 
+            countOfStars = 5;
+            break;
+        case 'B': 
+            countOfStars = 4;
+            break;
+        case 'C': 
+            countOfStars = 3;
+            break;
+        case 'D': 
+            countOfStars = 2;
+            break;
+        default:
+            break;
     }
-    else if (grade == 'B')
+    printf("Performance: ");
+    for(int index = 0; index < countOfStars; index++)
     {
-        printf("Performance : ****\n");
+        printf("*");
     }
-    else if (grade == 'C')
-    {
-        printf("Performance : ***\n");
-    }
-    else if (grade == 'D')
-    {
-        printf("Performance : **\n");
-    }
+    printf("\n");
 }
 
-void printRoll(struct studentDetails student[],int num,int index)
+void printRoll(struct studentDetails student[],int studentsCount,int index)
 {
-    if (index == num)
+    if (index == studentsCount)
     {
         return;
     }
     printf(" %d",student[index].roll_num);
-    printRoll(student,num,index+1);
+    printRoll(student,studentsCount,index+1);
 }
 
-void printStudentDetails(struct studentDetails *students,int num)
+void printStudentDetails(struct studentDetails *students,int studentsCount)
 {
     printf("\nStudent details: \n");
-    for (int index = 0; index < num; index++)
+    for (int index = 0; index < studentsCount; index++)
     {
         printf("\nRoll : %d\n",students[index].roll_num);
         printf("Name : %s\n",students[index].name);
@@ -104,20 +100,20 @@ void printStudentDetails(struct studentDetails *students,int num)
 int main()
 {
     struct studentDetails students[MAX_STUDENTS];
-    int num,index;
+    int studentsCount,index;
     printf("\nEnter number of students: ");
-    scanf("%d",&num);
-    for (int index = 0; index < num; index++)
+    scanf("%d",&studentsCount);
+    for (int index = 0; index < studentsCount; index++)
     {
         printf("Enter student %d details(roll,name,marks1,marks2,marks3): ",index+1);
-        scanf("%d %s %d %d %d",&students[index].roll_num,students[index].name,&students[index].marks1,&students[index].marks2,&students[index].marks3);
-        students[index].total = computeTotalMarks(students[index]);
-        students[index].average_marks = computeAverageMarks(students[index]);
+        scanf("%d %s %d %d %d",&students[index].roll_num,students[index].name,&students[index].marks[0],&students[index].marks[1],&students[index].marks[2]);
+        students[index].total = students[index].marks[0]+students[index].marks[1]+students[index].marks[2];
+        students[index].average_marks = students[index].total / 3.0;
         computeGrade(&students[index]);
     }
-    printStudentDetails(students,num);
+    printStudentDetails(students,studentsCount);
     printf("\nList of Roll Numbers (via recursion): ");
-    printRoll(students,num,0);
+    printRoll(students,studentsCount,0);
     printf("\n");
     return 0;
 }
